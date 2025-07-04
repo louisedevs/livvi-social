c07629c7f473c95286075db7b5da471de76280c3
louisedias <louisedias9@gmail.com>
2025-07-03 22:15:41 -0300

    home,login,bootstrap

--- livvi-social-frontend/src/frontend/app/app.component.ts
+++ livvi-social-frontend/src/frontend/app/app.component.ts
@@ -2,15 +2,22 @@ import { Component } from '@angular/core';
 import { provideRouter, RouterOutlet } from '@angular/router';
 import { routes } from './app.routes';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './home/home.component.html',
+  imports: [RouterOutlet,
+    CommonModule,
+    HomeComponent
+  ],
+
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
-})
+    
+  })
 export class AppComponent {
   protected title = 'livvi-social-frontend';
 }
--- livvi-social-frontend/src/frontend/app/home/home.component.ts
+++ livvi-social-frontend/src/frontend/app/home/home.component.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+// src/app/home/home.component.ts
 
+// --- Importações no topo do arquivo ---
+import { Component, OnInit, OnDestroy, Inject } from '@angular/core';
+import { CommonModule, DOCUMENT } from '@angular/common';
+
+// --- @Component decorator ---
 @Component({
   selector: 'app-home',
   standalone: true,
+  imports: [CommonModule],
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit, OnDestroy {
+  constructor(@Inject(DOCUMENT) private document: Document) {}
+  ngOnInit(): void {
+    this.document.body.classList.add('background-preto');
+  }
+
+  ngOnDestroy(): void {
+    this.document.body.classList.remove('background-preto');
+  }
+}
