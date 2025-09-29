7107a5eb2492bed76b81643bbe5a90cf1245e7c6
louisedias <louisedias9@gmail.com>
2025-09-29 20:48:08 -0300

    register e senha completos

---
+++ livvi-social-frontend/src/frontend/app/business/business.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-business',
+  imports: [],
+  templateUrl: './business.html',
+  styleUrl: './business.css'
+})
+export class Business {
+
+}
